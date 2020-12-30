d36d34da0a84d9ed48a8e9774361140b3bce5dd7
csgtdev04 <saitanujm@gmail.com>
2020-12-29 21:06:00 -0600

    front end & backend

---
+++ backend/todoproject/src/main/java/com/todoapp/todoproject/TodoProjectWithDbApplication.java
@@ -0,0 +1,14 @@
+package com.todoapp.todoproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoProjectWithDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoProjectWithDbApplication.class, args);
+	}
+
+
+}
